7208634134d0dd8b105363bd996c031e4df87895
zhuodongming <605395451@qq.com>
2017-07-27 21:40:49 +0800

    修改Log Helper

--- src/DM.Infrastructure/Helper/Log.cs
+++ src/DM.Infrastructure/Helper/Log.cs
@@ -13,49 +13,29 @@ namespace DM.Infrastructure.Helper
     /// </summary>
     public static class Log
     {
-        private readonly static ILog log = null;
+        private static ILog log = null;
         static Log()
         {
             ILoggerRepository repository = LogManager.CreateRepository("DefaultRepository");
-            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+            XmlConfigurator.ConfigureAndWatch(repository, new FileInfo("log4net.config"));
             log = LogManager.GetLogger(repository.Name, "DefaultLogger");
         }
 
-        /// <summary>
-        /// Debug
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="e"></param>
         public static void Debug(object message, Exception exception = null)
         {
             log.Debug(message, exception);
         }
 
-        /// <summary>
-        /// Info
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="e"></param>
         public static void Info(object message, Exception exception = null)
         {
             log.Info(message, exception);
         }
 
-        /// <summary>
-        /// Warn
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="e"></param>
         public static void Warn(object message, Exception exception = null)
         {
             log.Warn(message, exception);
         }
 
-        /// <summary>
-        /// Error
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="e"></param>
         public static void Error(object message, Exception exception = null)
         {
             log.Error(message, exception);
