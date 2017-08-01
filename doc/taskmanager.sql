0614e01ba4530ba38363806c313e8926a1393de1
zhuodongming <605395451@qq.com>
2017-08-01 14:16:30 +0800

    修改数据库建表文档

--- doc/taskmanager.sql
+++ doc/taskmanager.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50615
 File Encoding         : 65001
 
-Date: 2017-07-25 17:23:24
+Date: 2017-08-01 14:14:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,11 +24,12 @@ CREATE TABLE `task` (
   `TaskName` varchar(100) NOT NULL COMMENT '任务名称',
   `CronExpressionString` varchar(255) NOT NULL COMMENT '运行频率设置',
   `CronRemark` varchar(255) NOT NULL COMMENT '任务运频率中文说明',
-  `AssemblyName` varchar(255) NOT NULL COMMENT '任务所在DLL对应的程序集名称',
-  `ClassName` varchar(255) NOT NULL COMMENT '任务所在类',
+  `AssemblyFullName` varchar(255) NOT NULL COMMENT '任务所在DLL对应的程序集名称',
+  `ClassFullName` varchar(255) NOT NULL COMMENT '任务所在类',
   `LastRunTime` datetime DEFAULT NULL COMMENT '上一次运行时间',
   `NextRunTime` datetime DEFAULT NULL COMMENT '下一次运行时间',
   `Status` tinyint(4) NOT NULL COMMENT '状态',
+  `IsEnabled` tinyint(4) NOT NULL COMMENT '是否启用',
   `Remark` varchar(1000) NOT NULL COMMENT '备注',
   `CreateTime` datetime NOT NULL COMMENT '创建时间',
   `ModifyTime` datetime DEFAULT NULL COMMENT '修改时间',
@@ -38,4 +39,4 @@ CREATE TABLE `task` (
 -- ----------------------------
 -- Records of task
 -- ----------------------------
-INSERT INTO `task` VALUES ('Test2017', '测试任务,输出当前时间', '0/10 * * * * ?', '每10秒运行一次', 'DM.Task', 'DM.Task.TaskSet.TestJob', null, null, '0', ' ', '2017-07-25 00:00:00', null);
+INSERT INTO `task` VALUES ('Test2017', '测试任务,输出当前时间', '0/10 * * * * ?', '每10秒运行一次', 'DM.Task', 'DM.Task.TaskSet.TestJob', null, null, '0', '1', ' ', '2017-07-25 00:00:00', null);
